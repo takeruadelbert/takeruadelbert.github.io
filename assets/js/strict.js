879c16871510555115cccf5c60c7740e0f04986a
Lukas Fam <takeruadelbert@gmail.com>
2021-11-18 15:00:43 +0700

    added features to disable right click mouse and inspect element

---
+++ assets/js/strict.js
@@ -0,0 +1,26 @@
+// Disable Inspect Element
+document.onkeydown = function (e) {
+    if (event.keyCode == 123) {
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+        return false;
+    }
+    if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+        return false;
+    }
+}
+
+// Disable Right-Click Mouse
+document.onmousedown = disableclick;
+status = "Right Click Disabled";
+
+function disableclick(event) {
+    if (event.button == 2) {
+        alert(status);
+        return false;
+    }
+}
